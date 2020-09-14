cbe0d5a826075d4bf61832542e27efabf4a69447
ancahuiban <ancahuiban3@gmail.com>
2020-09-14 12:45:54 +0300

    Modify server

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const port = process.env.PORT || 8080;
 const app = express();
 const favicon = require("express-favicon");
 
-app.use(favicon(__dirname + "/public/favicon.ico"));
+app.use(favicon(__dirname + "/build/favicon.ico"));
 app.use(express.static(__dirname));
-app.use(express.static(__dirname + "/public"));
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "/index.html"));
+app.use(express.static(path.join(__dirname, "build")));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 app.listen(port, () => {
   console.log(`App started at port ${port}`);
