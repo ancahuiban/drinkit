ab10cda72113949c1b67a75dc117f95a282a2c3f
ancahuiban <ancahuiban3@gmail.com>
2020-09-14 12:39:45 +0300

    Add favico n

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,30 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Tab, MainButton, IconContainer } from "./style";
 import { mainMenu } from "./constants";
 import styled from "styled-components";
 
-export const MenuTabs = () => (
-  <Container>
-    {mainMenu.map(({ name, Icon, link }, id) => {
-      const StyledIcon = styled(Icon)`
-        width: 40px;
-        height: 40px;
-      `;
+export const MenuTabs = () => {
+  return (
+    <Container>
+      {mainMenu.map(({ name, Icon, link }, id) => {
+        const StyledIcon = styled(Icon)`
+          width: 40px;
+          height: 40px;
+        `;
 
-      if (!name) {
+        if (!name) {
+          return (
+            <MainButton href={link}>
+              <StyledIcon />
+            </MainButton>
+          );
+        }
         return (
-          <MainButton href={link}>
-            <StyledIcon />
-          </MainButton>
+          <Tab key={id} href={link}>
+            <IconContainer>
+              <StyledIcon />
+            </IconContainer>
+          </Tab>
         );
-      }
-      return (
-        <Tab key={id} href={link}>
-          <IconContainer>
-            <StyledIcon />
-          </IconContainer>
-        </Tab>
-      );
-    })}
-  </Container>
-);
+      })}
+    </Container>
+  );
+};
