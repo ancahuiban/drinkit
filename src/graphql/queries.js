a62b0ea68e9821893b17452ea92ae690e1df5226
ancahuiban <ancahuiban3@gmail.com>
2020-11-02 22:08:51 +0200

    Add graphql client and queries

---
+++ src/graphql/queries.js
@@ -0,0 +1,73 @@
+import gql from "graphql-tag";
+
+export const getBeverages = gql`
+  query {
+    getBeverages {
+      id
+      name
+      beverageTypeID
+      assortmentID
+      origin
+      edition
+      alcoholPercentage
+      score
+    }
+  }
+`;
+
+export const getBeverage = gql`
+  query {
+    getBeverage {
+      id
+      name
+      beverageTypeID
+      assortmentID
+      origin
+      edition
+      alcoholPercentage
+      score
+    }
+  }
+`;
+
+export const getBeveragesTypes = gql`
+  query {
+    getBeverageTypes {
+      id
+      name
+      assortments {
+        name
+      }
+    }
+  }
+`;
+
+export const getBeverageType = gql`
+  query {
+    getBeverageType {
+      id
+      name
+      assortments {
+        name
+      }
+    }
+  }
+`;
+
+export const getAssortments = gql`
+  query {
+    getAssortments {
+      id
+      name
+    }
+  }
+`;
+
+export const getAssortment = gql`
+  query {
+    getAssortment {
+      id
+      name
+    }
+  }
+`;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// import { ApolloProvider } from "react-apollo";
-// import { ApolloClient } from "apollo-client";
-// import { InMemoryCache } from "apollo-cache-inmemory";
-// import { HttpLink } from "apollo-link-http";
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
 
 import "./index.css";
 import { theme } from "./theme";
@@ -15,25 +15,25 @@ import { BeerList, Dashboard, AddBeer } from "./screens";
 import { PageContainer } from "./PageContainer";
 
 const App = () => {
-  // const client = new ApolloClient({
-  //   link: new HttpLink({ uri: "http://localhost:4000/graphql" }),
-  //   cache: new InMemoryCache(),
-  // });
+  const client = new ApolloClient({
+    link: new HttpLink({ uri: "http://localhost:8000/graphql" }),
+    cache: new InMemoryCache(),
+  });
 
   return (
-    // <ApolloProvider client={client}>
-    <ThemeProvider theme={theme}>
-      <Router>
-        <PageContainer>
-          <Switch>
-            <Route exact path="/" component={Dashboard} />
-            <Route path="/see-beers" component={BeerList} />
-            <Route path="/add-beer" component={AddBeer} />
-          </Switch>
-        </PageContainer>
-      </Router>
-    </ThemeProvider>
-    // </ApolloProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <PageContainer>
+            <Switch>
+              <Route exact path="/" component={Dashboard} />
+              <Route path="/see-beers" component={BeerList} />
+              <Route path="/add-beer" component={AddBeer} />
+            </Switch>
+          </PageContainer>
+        </Router>
+      </ThemeProvider>
+    </ApolloProvider>
   );
 };
 
