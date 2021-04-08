f4087b910679d09f2843f6c4eaf0243593975dd1
Mihail <mihail@dotts.ro>
2021-04-08 14:29:30 +0300

    Serve app through express server

--- packages/server/index.js
+++ packages/server/index.js
@@ -1,7 +1,9 @@
 const cors = require("cors");
+const path = require("path");
 const dotenv = require("dotenv");
 const express = require("express");
 const mongoose = require("mongoose");
+const favicon = require("express-favicon");
 
 const bottlecapRouter = require("./routes/bottlecap");
 
@@ -11,6 +13,16 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/bottlecap", bottlecapRouter);
+
+if (process.env.NODE_ENV === 'production') {
+  console.log('Serving content.')
+  app.use(favicon(__dirname + "../client/build/favicon.ico"));
+  app.use(express.static(__dirname));
+  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+  });
+}
 
 mongoose.connect(
   process.env.DATABASE_URL,
